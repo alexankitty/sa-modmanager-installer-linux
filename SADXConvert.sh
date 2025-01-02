99d12f280be1884591fe46df3c251b8b18ff84d3
Alexandra <alexankitty@gmail.com>
2025-01-02 14:28:21 -0700

    remove file after download

--- SADXConvert.sh
+++ SADXConvert.sh
@@ -4,6 +4,7 @@ export WINEPREFIX=$HOME/.local/share/samodmanager
 sdaxmi="https://dcmods.unreliable.network/owncloud/data/PiKeyAr/files/Setup/sadx_setup.exe"
 curl -Lso /tmp/sadx_setup.exe $sdaxmi
 cp /tmp/sadx_setup.exe "${WINEPREFIX}/drive_c/SAModManager"
+rm /tmp/sadx_setup.exe
 
 #Launch
 wine "${WINEPREFIX}/drive_c/SAModManager/sadx_setup.exe"
